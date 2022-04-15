a1fe88e8b763f8a5cc5a8e2d09fe6a2b1a8e2cf6
PreetiSharma15 <pwhitehat6@gmail.com>
2022-04-15 17:01:51 +0530

    Data CSV Added

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # PRO-C128-Project-Solution
 
-# IMP NOTE: The page at the given URL is maintained by "wikipedia", which might be updated in future, hence the current might be different.
-# Perform data scraping from scratch with HTML Tags/Class Names!
+* IMP NOTE: The page at the given URL is maintained by "wikipedia", which might be updated in future, hence the current might be different.
+
+* Perform data scraping from with correct HTML Tags/Class Names or Index!
--- brown_dwarfs_stars_data_scraping.py
+++ brown_dwarfs_stars_data_scraping.py
@@ -2,20 +2,32 @@ from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
 
-# IMP NOTE: The page at the given URL is maintained by "wikipedia", which might be updated in future, hence the current might be different.
+# IMP NOTE: The page at the given URL is maintained by "wikipedia", which might be updated in future, hence the current data might be different.
 # Perform data scraping from scratch with HTML Tags/Class Names!
 
+
+# URLto Scrape Data
 url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
 
+# Get Page
 page = requests.get(url)
 
+# Parse Page
 soup = bs(page.text,'html.parser')
 
-star_table = soup.find_all('table')
+# Get <table> with class = 'wikitable sortable'
+star_table = soup.find_all('table', {"class":"wikitable sortable"})
+
+total_table = len(star_table)
+
 
 temp_list= []
 
-table_rows = star_table[7].find_all('tr')
+# IMP NOTE: The page at the given URL is maintained by "wikipedia", which might be updated in future.
+# Hence check the index number poperly for star_table[1]
+# Currently, there are there 3 table with class = "class":"wikitable sortable" and "Field brown dwarfs" Table is the 2nd table
+# Thus the index is 1
+table_rows = star_table[1].find_all('tr')
 
 for tr in table_rows:
     td = tr.find_all('td')
@@ -27,6 +39,7 @@ Distance =[]
 Mass = []
 Radius =[]
 
+print(temp_list)
 
 for i in range(1,len(temp_list)):
     
@@ -35,7 +48,9 @@ for i in range(1,len(temp_list)):
     Mass.append(temp_list[i][7])
     Radius.append(temp_list[i][8])
 
+# Convert to CSV
+headers = ['Star_name','Distance','Mass','Radius']  
 df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
 print(df2)
 
-df2.to_csv('dwarf_stars.csv')
+df2.to_csv('dwarf_stars.csv', index=True, index_label="id")
