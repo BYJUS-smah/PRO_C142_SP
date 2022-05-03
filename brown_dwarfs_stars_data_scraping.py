817c049ce67e89f1ec7021b2b833031f55013bd6
bjsfsmx-0544 <94998868+bjsfsmx-0544@users.noreply.github.com>
2022-05-03 16:09:02 -0500

    Traducción 1.00

--- brown_dwarfs_stars_data_scraping.py
+++ brown_dwarfs_stars_data_scraping.py
@@ -2,20 +2,20 @@ from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
 
-# IMP NOTE: The page at the given URL is maintained by "wikipedia", which might be updated in future, hence the current data might be different.
-# Perform data scraping from scratch with HTML Tags/Class Names!
+# Nota importante: la página dada en el URL se mantiene por "wikipedia", por lo que puede ser actualizada en el futurowhich might be updated in future, por lo que los datos actuales pueden ser diferentes
+# ¡Realiza la extracción de datos desde cero utilizando etiquetas HTML / nombres de clases!
 
 
-# URLto Scrape Data
+# URL para extraer datos
 url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
 
-# Get Page
+# Obtener la página
 page = requests.get(url)
 
-# Parse Page
+# Analizar la página
 soup = bs(page.text,'html.parser')
 
-# Get <table> with class = 'wikitable sortable'
+# Obtener <table> con class = 'wikitable sortable'
 star_table = soup.find_all('table', {"class":"wikitable sortable"})
 
 total_table = len(star_table)
@@ -23,10 +23,10 @@ total_table = len(star_table)
 
 temp_list= []
 
-# IMP NOTE: The page at the given URL is maintained by "wikipedia", which might be updated in future.
-# Hence check the index number poperly for star_table[1]
-# Currently, there are there 3 table with class = "class":"wikitable sortable" and "Field brown dwarfs" Table is the 2nd table
-# Thus the index is 1
+# Nota importante: la página en el enlace dado es mantenida por "wikipedia" y puede ser actualizada en el futuro
+# Por lo tanto, verifica la propiedad del número de índice para star_table[1]
+# Actualmente, sólo hay tres tablas con class = "class":"wikitable sortable" y la tabla "Field brown dwarfs" es la segunda tabla
+# Por lo que el índice es 1
 table_rows = star_table[1].find_all('tr')
 
 for tr in table_rows:
@@ -48,7 +48,7 @@ for i in range(1,len(temp_list)):
     Mass.append(temp_list[i][7])
     Radius.append(temp_list[i][8])
 
-# Convert to CSV
+# Convertir a CSV
 headers = ['Star_name','Distance','Mass','Radius']  
 df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
 print(df2)
